eaf6cab8f576bee647ad37a5075cd3ce9fa6190b
Ankit Kumar Sharma <75676084+Ankit-604@users.noreply.github.com>
2024-09-30 20:43:20 +0530

    Add files via upload

---
+++ Assingnment06/switch/7-switch.js
@@ -0,0 +1,29 @@
+//Write a switch case to print the name of a day (e.g., 1 for Sunday, 2 for Monday, etc.) based on a number input.4.
+
+let day = 5;
+
+switch (day) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid day");
+}
