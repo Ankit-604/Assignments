4aa6e392df8f1f583fab7242d802b7a7f4a236ed
Ankit Kumar Sharma <75676084+Ankit-604@users.noreply.github.com>
2024-09-30 20:30:13 +0530

    Add files via upload

---
+++ Assingnment06/Area/3-Area.js
@@ -0,0 +1,7 @@
+//Write a program to calculate the area of a rectangle using variables length and width.
+
+let length = 10;
+let width = 5;
+
+let area = length * width;
+console.log("Area of rectangle is " + area);
