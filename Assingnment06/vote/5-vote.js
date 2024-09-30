5e6c0a82056e26bb9e1205abed94f59eacb78d44
Ankit Kumar Sharma <75676084+Ankit-604@users.noreply.github.com>
2024-09-30 20:32:49 +0530

    Add files via upload

---
+++ Assingnment06/vote/5-vote.js
@@ -0,0 +1,9 @@
+//Write a program to determine if a person is eligible to vote based on their age. The eligible age is 18 and above.
+
+let age = 10;
+
+if (age >= 18) {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible to vote");
+}
