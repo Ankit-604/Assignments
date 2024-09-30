58b198b4a16e0acc85a48ba79b8bdf4d71b56d32
Ankit Kumar Sharma <75676084+Ankit-604@users.noreply.github.com>
2024-09-30 20:24:55 +0530

    Add files via upload

---
+++ Assingnment06/variable/1-variables.js
@@ -0,0 +1,10 @@
+// Question 1
+// Create variables to store your name, age, and whether you are a student (boolean). Log these variables to the console.
+
+let name = "Ankit Kumar Sharma";
+const age = 25;
+const isStudent = true;
+
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Student: ", isStudent);
