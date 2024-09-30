f8db3f59d526513a19771fdefa7ee071c8871b2f
Ankit Kumar Sharma <75676084+Ankit-604@users.noreply.github.com>
2024-09-30 20:47:48 +0530

    Add files via upload

---
+++ Assingnment06/ternaryOperator/12-ternaryOperator.js
@@ -0,0 +1,9 @@
+//Write a program to check if a number is positive, negative, or zero using the ternary operator
+
+let number = 0;
+
+number > 0
+  ? console.log("Positive")
+  : number < 0
+  ? console.log("Negative")
+  : console.log("Zero");
